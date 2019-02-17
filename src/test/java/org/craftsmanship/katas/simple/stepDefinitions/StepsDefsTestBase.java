2ea3c132af84bbfd4d63835d17980684554466ff
Steeves Saillant <steevessaillant@hotmail.com>
2019-02-16 20:57:48 -0500

    optimize spring boot for speedy test execution add @Lazy

--- src/test/java/org/craftsmanship/katas/simple/stepDefinitions/StepsDefsTestBase.java
+++ src/test/java/org/craftsmanship/katas/simple/stepDefinitions/StepsDefsTestBase.java
@@ -5,12 +5,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @BootstrapWith(SpringBootTestContextBootstrapper.class)
 @SpringBootTest(classes = StepsDefsTestBase.class)
+@Lazy
 @Configuration
 @ComponentScan(basePackages = { "org.craftsmanship.katas.simple.stepDefinitions" })
 public abstract class StepsDefsTestBase extends SpringBootTestContextBootstrapper {
