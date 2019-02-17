b4e4612231867137c0471e88ad28c05b5afdb902
Steeves Saillant <steevessaillant@hotmail.com>
2019-02-16 20:50:57 -0500

    optimize spring boot for speedy test execution

--- src/test/java/org/craftsmanship/katas/simple/stepDefinitions/StepsDefsTestBase.java
+++ src/test/java/org/craftsmanship/katas/simple/stepDefinitions/StepsDefsTestBase.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.BootstrapWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @BootstrapWith(SpringBootTestContextBootstrapper.class)
 @SpringBootTest(classes = StepsDefsTestBase.class)
 @Configuration
